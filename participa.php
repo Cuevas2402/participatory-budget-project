6977dc822f354e7c21a2729b6bdea65b5da3d3b4
Josemgallegos <126370753+Josemgallegos@users.noreply.github.com>
2023-04-26 20:28:28 -0600

    Update participa.php

--- participa.php
+++ participa.php
@@ -1,13 +1,11 @@
-    <?php
-        require 'config/db.php';
-        $db = new Database();
-        $pdo = $db -> connect();
-        $stmt = $pdo->prepare("CALL get_process_featured_2()");
-        $stmt->execute();
-        $rows = $stmt->fetchAll();
-    ?>
-
-
+<?php
+  require 'config/db.php';
+  $db = new Database();
+  $pdo = $db -> connect();
+  $stmt = $pdo->prepare("CALL get_process_featured_2()");
+  $stmt->execute();
+  $rows = $stmt->fetchAll();
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -110,68 +108,196 @@
             </div> 
         </div>
 
-  <!-- Start First Content -->
-  <div class="container process-content">
-    <div class="row text-center">
-        <div class="col">
-            <p>ÁMBITOS <select  class="process-select" type="text">
-            <option value="Selecciona un ámbito" selected disabled>Selecciona un ámbito</option>
-            </select></p>
-        </div>
-        <div class="col">
-            <p>ÁREAS <select style="margin-left: 19px;" class="process-select" type="text">
-            <option value="Selecciona un ámbito" selected disabled>Selecciona un área</option></select></p>
-        </div>
-    </div>
+        <!-- Start First Content -->
+        <div class="container process-content">
+            <div class="row text-center">
+                <div class="col">
+                    <p>ÁMBITOS 
+                        <select  id="ambitos"  name="ambitos" class="process-select" type="text">
+                            <option value="0" selected>Todos</option>
+                            <?php
+                                $stmt = $pdo->prepare("CALL get_ambitos()");
+                                $stmt->execute();
+                                $rows = $stmt->fetchAll();
+                                foreach($rows as $row){
+                                    ?>
+                                        <option value="<?php echo $row['aid']?>"><?php echo $row['nombre_ambito'] ?></option>
+                                    <?php
 
-        <div class="container text-center" style="margin-top: 1rem;">
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
-                <div class="col g-5">
-                    <div class="row">
-                        <span class="process-line"></span>
-                    </div>
-                    <div class="row">
-                        <div class="col border" style="padding-bottom: 1rem; padding-right: 0; padding-left: 0;">
-                            <!-- Imagen de 200 x 100 -->
-                            <img class="w-100" src="http://drive.google.com/uc?export=view&id=1Bw22s4t6l_H6e9r6f_A7y0jIuGYEeRy0" alt="">
-                            <h5 class="process-title-card"><span>Titulo</span><!--<i class="fa fa-bell process-bell-active" aria-hidden="true"></i>--></h5>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col border">
-                            <p class="process-date-card"><strong>Ambito:</strong> aaa</p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col border">
-                            <p class="process-date-card"><strong>Distrito:</strong> aaaa</p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col border">
-                            <p class="process-date-card"><strong>Fecha de inicio</strong><br>8/11/2002</p>
-                        </div>
-                        <div class="col border">
-                            <p class="process-date-card"><strong>Fecha de finalización</strong><br>8/11/2002</p>
-                        </div>
-                    </div>
-                    <div class="row border">
-                        <div class="col" style="background-color:#EAD9D8">
-                            <p class="process-status-card"><strong>Fase actual</strong></p>
-                            <center><button class="process-button">Introducción</button></center>
-                            <a href="./participa2.php"><button class="process-button-card"><strong>Más información</strong></button></a>
-                        </div>
-                    </div>
-                </div> 
+                                }
+                                $stmt->closeCursor();
+                            ?>
+                        </select>
+                    </p>
+                </div>
 
+                <div class="col">
+                    <p>DISTRITOS 
+                        <select  id="distritos" name="distritos" class="process-select" type="text">
+                            <option value="0" selected>Todos</option>
+                            <?php
+                                $stmt = $pdo->prepare("CALL get_distritos()");
+                                $stmt->execute();
+                                $rows = $stmt->fetchAll();
+                                foreach($rows as $row){
+                                    ?>
+                                        <option value="<?php echo $row['did']?>" ><?php echo $row['nombre_distrito'] ?></option>
+                                    <?php
+
+                                }
+                                $stmt->closeCursor();
+                            ?>
+                        </select>
+                    </p>
+                </div>
+                
+            </div>
+
+            <div class="container text-center" style="margin-top: 1rem;">
+                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+
+                    <?php
+                        $stmt = $pdo->prepare("CALL get_process_card()");
+                        $stmt->execute();
+                        $rows = $stmt->fetchAll();
+                        foreach($rows as $row){
+
+                    ?>
+                            <div class="col g-5">
+                                <div class="row">
+                                    <span class="process-line"></span>
+                                </div>
+                                <div class="row">
+                                    <div class="col border" style="padding-bottom: 1rem; padding-right: 0; padding-left: 0;">
+                                        <!-- Imagen de 200 x 100 -->
+                                        <img class="w-100" src="http://drive.google.com/uc?export=view&id=1Bw22s4t6l_H6e9r6f_A7y0jIuGYEeRy0" alt="">
+                                        <h5 class="process-title-card"><span><?php echo $row['titulo_proceso'];?></span><!--<i class="fa fa-bell process-bell-active" aria-hidden="true"></i>--></h5>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col border">
+                                        <p class="process-date-card"><strong>Ambito:</strong> <?php echo $row['nombre_ambito'];?></p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col border">
+                                        <p class="process-date-card"><strong>Distrito:</strong> <?php echo $row['nombre_distrito'];?></p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col border">
+                                        <p class="process-date-card"><strong>Fecha de inicio</strong><br><?php echo $row['fecha_inicio_proceso'];?></p>
+                                    </div>
+                                    <div class="col border">
+                                        <p class="process-date-card"><strong>Fecha de finalización</strong><br> <?php echo $row['fecha_fin_proceso'];?> </p>
+                                    </div>
+                                </div>
+                                <div class="row border">
+                                    <div class="col" style="background-color:#EAD9D8;">
+                                        <p class="process-status-card"><strong>Fase actual</strong></p>
+                                        <center><button class="process-button"><?php echo $row['titulo_fase'];?></button></center>
+                                        <a href="./participa2.php"><button class="process-button-card"><strong>Más información</strong></button></a>
+                                    </div>
+                                </div>
+                            </div> 
+                    <?php
+                        }
+                        $stmt->closeCursor();
+                    ?>
+
+                    
+                </div>
             </div>
         </div>
-  </div>
-  <!-- End First Content -->
+        <!-- End First Content -->
 
         <hr style="width: 75%; margin: 0 auto;">
 
+<<<<<<< HEAD
+  <!-- process dps -->
+
+  <!-- Process -->
+  <div class="container process-featured">
+    <div class="process-filter">
+        <h4><strong><i class="fa fa-square" aria-hidden="true"></i> PROCESOS (#)</strong></h4>
+        <h4 class="see-process">VER</h4>
+        <h4><a href="">ACTIVOS (#)</a></h4>
+        <h4><a href="">PASADOS (#)</a></h4>
+        <h4><a href="">TODOS (#)</a></h4>
+    </div> 
+  </div>
+
+  <!-- Start First Content -->
+    <div class="container process-content">
+        <div class="row text-center">
+            <div class="col">
+                <p>ÁMBITOS <select  class="process-select" type="text">
+                <option value="Selecciona un ámbito" selected disabled>Selecciona un ámbito</option>
+                </select></p>
+            </div>
+            <div class="col">
+                <p>ÁREAS <select style="margin-left: 19px;" class="process-select" type="text">
+                <option value="Selecciona un ámbito" selected disabled>Selecciona un área</option></select></p>
+            </div>
+        </div>
+
+        <div class="container text-center" style="margin-top: 1rem;">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 d-flex align-items-stretch g-3">
+                <div class="col col-lg-3">
+                    <div class="card h-100">
+                        <div class="card-header process-line"></div>
+                        <img src="http://drive.google.com/uc?export=view&id=1Bw22s4t6l_H6e9r6f_A7y0jIuGYEeRy0" class="card-img-top" alt="...">
+                        <div class="card-body" style="padding: 0;">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item d-flex align-items-center" style="height: 100px">
+                                    <h5 class="process-title-card">El titulo debería tener wrap</h5>
+                                </li>
+                                <li class="list-group-item">
+                                    <p class="process-content-card"><strong>Ambito:</strong> aaaa</p> 
+                                </li>
+                                <li class="list-group-item">
+                                    <p class="process-content-card"><strong>Ambito:</strong> aaaa</p>
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="row align-items-center">
+                                        <div class="col">
+                                            <p class="process-date-card"><strong>Fecha de inicio</strong></p>
+                                        </div>
+                                        <div class="col">
+                                            <p class="process-date-card"><strong>Fecha de finalización</strong></p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <p class="process-date-card">8/11/2002</p>
+                                        </div>
+                                        <div class="col">
+                                            <p class="process-date-card">8/11/2002</p>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="list-group-item d-flex flex-column" style="background-color: #ead9d8">
+                                    <p class="process-status-card"><strong>Fase actual</strong></p>
+                                    <button class="process-button">Introducción</button>
+                                    <a href="#"><button class="process-button-card"><strong>Más información</strong></button></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  <!-- End First Content -->
+
+  <hr style="width: 75%; margin: 0 auto;">
+
+  <!-- Past Process -->
+  
+  <!-- End Second Content -->
+=======
         <!-- Past Process -->
+>>>>>>> ee3bfebf27cd318487ba87dd5e267a1a3ad74304
     
         <!-- End Second Content -->
         
