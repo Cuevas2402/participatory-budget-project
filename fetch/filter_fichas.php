04339ab5845f2159924b3dc8a670393af0a2bb8a
cuevas <juan.m.cuevas@outlook.com>
2023-05-10 22:08:50 -0600

    Finish fichasActivas filter

--- fetch/filter_fichas.php
+++ fetch/filter_fichas.php
@@ -18,23 +18,41 @@
                 require '../components/card_ficha.php';
             }
         }
-    }
-    if(isset($_POST['datos']) ){
-        $datos = $_POST['datos'];
+    }else{
 
-        $placeholders = implode(',', array_fill(0, count($datos), '?'));
-        $query = "SELECT *, participaciones.img as imagen FROM participaciones, usuarios, distritos WHERE usuarios.uid = participaciones.uid and distritos.did = participaciones.did AND participaciones.did IN ($placeholders)";
-        $params = $datos;
 
+    
+        $query = "SELECT *, participaciones.img as imagen FROM participaciones, usuarios, distritos, votos WHERE usuarios.uid = participaciones.uid and distritos.did = participaciones.did AND participaciones.did AND votos.pid = participaciones.pid AND votos.voted = participaciones.uid";
+
+        $params = array();
+
+        if(!empty($_POST['datos'])){
+            $datos = $_POST['datos'];
+            $placeholders = implode(',', array_fill(0, count($datos), '?'));
+            $query .= " AND participaciones.did IN ($placeholders)";
+            $params = array_merge($params, $datos);
+        }
+
+        if(!empty($_POST['datos2'])){
+            $datos2 = $_POST['datos2'];
+            $placeholders = implode(',', array_fill(0, count($datos2), '?'));
+            $query .= " AND participaciones.estatus IN ($placeholders)";
+            $params = array_merge($params, $datos2);
+        }
+
+        if ($_POST['select'] == 2) {
+            $query .= " ORDER BY participaciones.fecha_creacion ASC";
+        }
+
+        if ($_POST['select'] == 3) {
+            $query .= " GROUP BY participaciones.pid, participaciones.uid ORDER BY COUNT(voted) DESC";
+        }
         $sql = $pdo->prepare($query);
         $sql->execute($params);
+
         $rows = $sql->fetchAll(PDO::FETCH_ASSOC);
         foreach($rows as $row){
             require '../components/card_ficha.php';
         }
-        $sql -> closeCursor();
-        
-
-        
     }
 ?>
